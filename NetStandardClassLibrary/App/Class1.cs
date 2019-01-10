100ef71acf826b8dbcc1056c1f1509d75796bb62
Klaus Barkhausen <klaus@barkhausen.us>
2019-01-10 15:24:06 -0500

    A set of project created from default .NET templates

---
+++ NetStandardClassLibrary/App/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace App
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            var x = JsonConvert.DeserializeObject<Person>("{ 'firstName' : 'Ted' }");
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+    }
+}
